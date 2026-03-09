9c026fadb5f3cedcdddf50b0407f910ab25ffc42
Akanov222 <akanov.gazproekt@gmail.com>
2026-03-09 23:52:49 +0300

    mvp-2

--- src/main/java/com/example/todoapp/repositories/TodoItemRepository.java
+++ src/main/java/com/example/todoapp/repositories/TodoItemRepository.java
@@ -4,4 +4,5 @@ import com.example.todoapp.domain.TodoItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TodoItemRepository extends JpaRepository<TodoItem, Long> {
+    Long id(Long id);
 }
